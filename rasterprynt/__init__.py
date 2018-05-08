57026f0a1c19692f1ecfcd257aa4baf48b9e2e6c
Philipp Hagemeister <phihag@phihag.de>
2018-05-08 22:11:43 +0200

    release 1.0.4

--- rasterprynt/__init__.py
+++ rasterprynt/__init__.py
@@ -18,7 +18,7 @@ except ImportError:  # Python 2
     from urllib2 import URLError
 
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 logger = logging.getLogger('rasterprynt')
 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='rasterprynt',
-      version='1.0.3',
+      version='1.0.4',
       description='Print raster graphics on Brother P950NW and 9800PCN',
       author='Boxine GmbH',
       author_email='it@boxine.de',
